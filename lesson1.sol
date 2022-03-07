6b81cb36d92b38862046ba550795616a22a9e327
Kyuma Morita <hokumon2@icloud.com>
2022-03-08 01:15:05 +0900

    lesson1 complete

--- lesson1.sol
+++ lesson1.sol
@@ -1,6 +1,8 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.19;
 
 contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -12,7 +14,19 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string _name, uint _dna) {
-        // do something
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna);
     }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
 }
